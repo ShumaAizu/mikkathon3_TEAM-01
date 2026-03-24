10df1aeebe6c867b0a463248a262f2000ac9220e
Shuma Aizu <251301@st.yoshida-g.ac.jp>
2026-03-24 17:35:33 +0900

    アイテム追加（でるだけ）

---
+++ item.cpp
@@ -0,0 +1,153 @@
+//=============================================================================
+//
+//	アイテム処理 [item.cpp]
+//	Author : SHUMA AIZU
+// 
+//=============================================================================
+
+#include "main.h"
+#include "item.h"
+#include "input.h"
+
+#include "debugproc.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define MAX_ITEM		(16)		// アイテムの最大数
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+Item g_aitem[MAX_ITEM];				// アイテムの情報
+int g_nNumItemData;					// アイテムデータ数
+int g_nNumItem;						// 現在のアイテム数
+
+//=============================================================================
+//	アイテムの初期化処理
+//=============================================================================
+void InitItem(void)
+{
+	// デバイスの取得
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	for (int nCntItem = 0; nCntItem < MAX_ITEM; nCntItem++)
+	{
+		g_aitem[nCntItem].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		g_aitem[nCntItem].rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		g_aitem[nCntItem].bUse = false;
+	}
+
+	g_nNumItem = 0;
+	g_nNumItemData = 0;
+
+	// test
+	//SetItem(D3DXVECTOR3(0.0f, 0.0f, 100.0f), INIT_D3DXVEC3, ITEMTYPE_000, MODELTYPE_001);
+}
+
+//=============================================================================
+//	アイテムの終了処理
+//=============================================================================
+void UninitItem(void)
+{
+
+}
+
+//=============================================================================
+//	アイテムの描画処理
+//=============================================================================
+void DrawItem(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();	// デバイスの取得
+	D3DXMATRIX mtxRot, mtxTrans, mtxScale;		// 計算用マトリックス
+	D3DMATERIAL9 matDef;						// 現在のマテリアル保存用
+	D3DXMATERIAL* pMat;							// マテリアルデータへのポインタ
+
+	Item* pItem = &g_aitem[0];
+
+	for (int nCntItem = 0; nCntItem < MAX_ITEM; nCntItem++, pItem++)
+	{
+		if (pItem->bUse == false)
+		{// 使われていなければ戻る
+			continue;
+		}
+		
+		// ワールドマトリックスの初期化
+		D3DXMatrixIdentity(&pItem->mtxWorld);
+
+		// 向きを反映
+		D3DXMatrixRotationYawPitchRoll(&mtxRot, pItem->rot.y, pItem->rot.x, pItem->rot.z);
+		D3DXMatrixMultiply(&pItem->mtxWorld, &pItem->mtxWorld, &mtxRot);
+
+		// 位置を反映
+		D3DXMatrixTranslation(&mtxTrans, pItem->pos.x, pItem->pos.y, pItem->pos.z);
+		D3DXMatrixMultiply(&pItem->mtxWorld, &pItem->mtxWorld, &mtxTrans);
+
+		// ワールドマトリックスの設定
+		pDevice->SetTransform(D3DTS_WORLD, &pItem->mtxWorld);
+
+		// 現在のマテリアルを取得
+		pDevice->GetMaterial(&matDef);
+
+		// マテリアルデータへのポインタを取得
+		pMat = (D3DXMATERIAL*)pItem->NormalObjectData.pModelData->pBuffMat->GetBufferPointer();
+
+		for (int nCntMat = 0; nCntMat < (int)pItem->NormalObjectData.pModelData->dwNumMat; nCntMat++)
+		{
+			// マテリアルの設定
+			pDevice->SetMaterial(&pMat[nCntMat].MatD3D);
+
+			// テクスチャの設定
+			pDevice->SetTexture(0, pItem->NormalObjectData.pModelData->apTexture[nCntMat]);
+
+			// アイテム(パーツ)の描画
+			pItem->NormalObjectData.pModelData->pMesh->DrawSubset(nCntMat);
+		}
+
+		// 保存していたマテリアルを戻す
+		pDevice->SetMaterial(&matDef);
+	}
+}
+
+//=============================================================================
+//	アイテムの更新処理
+//=============================================================================
+void UpdateItem(void)
+{
+
+}
+
+//=============================================================================
+//	アイテムの当たり判定処理
+//=============================================================================
+void CollisionItem(D3DXVECTOR3* pPos, float fRadius)
+{
+
+}
+
+//=============================================================================
+//	アイテムの設定処理
+//=============================================================================
+void SetItem(D3DXVECTOR3 pos, D3DXVECTOR3 rot, ITEMTYPE itemtype, MODELTYPE ModelType)
+{
+	Item* pItem = &g_aitem[0];
+
+	for (int nCntItem = 0; nCntItem < MAX_ITEM; nCntItem++, pItem++)
+	{
+		if (pItem->bUse == true)
+		{
+			continue;
+		}
+
+		pItem->pos = pos;
+		pItem->rot = rot;
+		pItem->itemtype = itemtype;
+
+		// モデルデータ設定
+		pItem->NormalObjectData.pModelData = SetModelData(ModelType);
+		pItem->bUse = true;
+
+		g_nNumItem++;
+		break;
+	}
+}
--- item.h
+++ item.h
@@ -1,12 +1,12 @@
 //=============================================================================
 //
-//	トラップ処理 [trap.h]
+//	アイテム処理 [item.h]
 //	Author : SHUMA AIZU
 // 
 //=============================================================================
 
-#ifndef _TRAP_H_
-#define _TRAP_H_
+#ifndef _ITEM_H_
+#define _ITEM_H_
 
 #include "main.h"
 #include "object.h"
@@ -16,39 +16,39 @@
 //*****************************************************************************
 
 //*****************************************************************************
-// トラップの種類
+// アイテムの種類
 //*****************************************************************************
-typedef enum TRAPTYPE
+typedef enum ITEMTYPE
 {
-	TRAPTYPE_000 = 0,
-	TRAPTYPE_001,
-	TRAPTYPE_002,
-	TRAPTYPE_MAX
-}TRAPTYPE;
+	ITEMTYPE_000 = 0,
+	ITEMTYPE_001,
+	ITEMTYPE_002,
+	ITEMTYPE_MAX
+}ITEMTYPE;
 
 //*****************************************************************************
-// トラップの構造体定義
+// アイテムの構造体定義
 //*****************************************************************************
-typedef struct Trap
+typedef struct Item
 {
 	D3DXVECTOR3 pos;							// 位置
 	D3DXVECTOR3 rot;							// 向き
 	D3DXVECTOR3 scale;							// 大きさ
 	D3DXMATRIX mtxWorld;						// ワールドマトリックス
-	TRAPTYPE traptype;							// トラップの種類
+	ITEMTYPE itemtype;							// アイテムの種類
 	Object_Normal NormalObjectData;				// 通常オブジェクトのデータ
 	int nCollisionIdx;							// 当たり判定のインデックス
 	bool bUse;									// 使用状態
-}Trap;
+}Item;
 
 //*****************************************************************************
 // プロトタイプ宣言
 //*****************************************************************************
-void InitTrap(void);
-void UninitTrap(void);
-void UpdateTrap(void);
-void DrawTrap(void);
-void CollisionTrap(D3DXVECTOR3* pPos, float fRadius);
-void SetTrap(D3DXVECTOR3 pos, D3DXVECTOR3 rot, TRAPTYPE traptype, MODELTYPE ModelType);
+void InitItem(void);
+void UninitItem(void);
+void UpdateItem(void);
+void DrawItem(void);
+void CollisionItem(D3DXVECTOR3* pPos, float fRadius);
+void SetItem(D3DXVECTOR3 pos, D3DXVECTOR3 rot, ITEMTYPE itemtype, MODELTYPE ModelType);
 
 #endif
